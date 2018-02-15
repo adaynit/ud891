8a8e50d4581ae0f3b89a9f0e57c86b6cdf071df8
adaynit <adaynit@yahoo.com>
2018-02-15 22:19:53 +0200

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,16 +1,49 @@
-    $(function() {
+   var listContainer = document.getElementById("list");
 
 
-   var people = [];
+ var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'http://api.jsonbin.io/b/5a7c24f404b9c21255e4298c');
+ourRequest.onload = function() {
+  var ourData = JSON.parse(ourRequest.responseText);
+  renderHTML(ourData);
+};
+ourRequest.send();   
 
-   $.getJSON('http://api.jsonbin.io/b/5a7c24f404b9c21255e4298c', function(data) {
-       $.each(data.person, function(i, f) {
-          var tblRow = "<tr>" + 
-           "<td>" + f.description + "</td>" + "<td>" + f.duration + ' hours' + "</td>" + "<td>" + f.date + "</td>" + "<td>" + f.time 
-           + "</td>" + "<td>" + f.city + "</td>" + "<td>" + f.location + "</td>" + "<td>" + f.image + "</td>" + "</tr>"
-           $(tblRow).appendTo('#userdata tbody'); 
-     });
 
-   });
 
-});
+function renderHTML(data){
+ 
+   
+ 
+  var htmlString = "";
+  
+  for(i = 0; i < data.length; i++){
+      var mydate = Date.parse(data[i].date).toString("dddd, MMMM dS, yyyy");
+      var time = $(data[i].time).val(); 
+        time = moment(time, "hh:mm A");
+       var myImg = new Image();
+       myImg.src = data[i].image;
+      
+      
+    
+     
+    
+
+
+      htmlString += '<h2 id="eventname">' + data[i].description + '</h2>' +
+                  '<h3>' + mydate + '</h3>' +
+                  '<p>' + data[i].time + '</p>' +
+               '<p>' + data[i].location + " " +data[i].city +'</p>' +
+               '<p>Interested? <a href="event1.html">click here</a> for more info</p>';
+                
+               
+   
+   
+  }
+    
+ listContainer.insertAdjacentHTML('beforeend', htmlString);
+    
+}
+
+  
+ 
